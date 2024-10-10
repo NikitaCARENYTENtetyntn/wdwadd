5169fbbe7cecdcaa330490132292fb72ff99f5cd
fuihwfuihw <carenkonikita6@gmail.com>
2024-10-10 21:17:06 +0900

    Update Lab  5.1.kt

--- Lab 4/Lab  5.1.kt
+++ Lab 4/Lab  5.1.kt
@@ -1,1 +1,13 @@
+fun processPayment(paymentMethod: String): String {
+    return when (paymentMethod) {
+        "наличные" -> "Оплачено наличными."
+        "кредитная карта" -> "Оплачено кредитной картой."
+        "PayPal" -> "Оплачено через PayPal."
+        else -> "Неизвестный способ оплаты."
+    }
+}
+fun main() {
+    val paymentMethod = readLine()!!
+    println(processPayment(paymentMethod))
+}
 
